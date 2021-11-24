2dcaaa809b9b3cdf9e9c53e79c51d370e421a080
josephhanke <jhanke@nescot.ac.uk>
2021-11-24 12:40:01 +0000

    Add project files.

---
+++ ExceptionHandling.Domain/GameActions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling.Domain
+{
+    public class GameActions
+    {
+
+        public static void Battle(Character attacker, Character defender)
+        {
+
+            Random random = new Random();
+            int attackValue = random.Next(100);
+
+            defender.HitPoints -= attackValue;
+
+        }
+
+
+
+
+    }
+}
